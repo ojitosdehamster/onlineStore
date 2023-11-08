2e27a4bb3b70f0f59966adb1ebb290aff63fcff4
OrellanaPc <astridofelia@gmial.com>
2023-11-07 23:48:33 -0600

    se agregó descripción del producto

--- src/main.js
+++ src/main.js
@@ -8,12 +8,13 @@ const orderHistory = document.getElementById('order-history');
 
 // Clase "Product" para representar productos
 class Product {
-    constructor(id, name, price, imageUrl) {
-      this.id = id;
-      this.name = name;
-      this.price = price;
-      this.imageUrl = imageUrl;
-    }
+  constructor(id, name, description, price, imageUrl) {
+    this.id = id;
+    this.name = name;
+    this.description = description;
+    this.price = price;
+    this.imageUrl = imageUrl;
+  }
   
     generateProductCard() {
       const card = document.createElement('div');
